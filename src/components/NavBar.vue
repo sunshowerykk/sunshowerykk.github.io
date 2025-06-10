4313e9e1d4bbbec75f6edeffb053db8b6499b5c5
zhangwenfeng <zhangwenfeng@haizhi.com>
2025-06-10 17:21:59 +0800

    feat: 项目初始化

---
+++ src/components/NavBar.vue
@@ -0,0 +1,8 @@
+<template>
+  <nav class="bg-white shadow p-4 flex justify-between">
+    <div class="text-xl font-bold">丁兴建</div>
+    <div class="space-x-4">
+      <router-link to="/" class="hover:underline">首页</router-link>
+    </div>
+  </nav>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<template>
+  <section class="max-w-3xl mx-auto mt-12 text-center">
+    <img src="https://avatars.githubusercontent.com/u/9919?s=280&v=4" alt="avatar" class="w-32 h-32 mx-auto rounded-full shadow-lg" />
+    <h1 class="text-3xl font-bold mt-4">张文凤</h1>
+    <p class="text-gray-600 mt-2">北京林业大学 · 计算机硕士 · 知识图谱 · 智能问答</p>
+    <p class="mt-4 text-lg">欢迎访问我的个人科研主页！</p>
+  </section>
+</template>
